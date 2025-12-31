995beb6f24b0defb9bf67ad3e40eff1d0f1c79fa
JosueOrd <josueg.ordonez@ucuenca.edu.ec>
2025-12-31 16:11:03 -0500

    Isevicio con funciones a revisar

--- src/main/java/com/reservaVuelos/servicio/IServicio.java
+++ src/main/java/com/reservaVuelos/servicio/IServicio.java
@@ -1,6 +1,12 @@
 package com.reservaVuelos.servicio;
 
+import com.reservaVuelos.servicio.DTOs.UsuarioDTO;
+
 public interface IServicio {
 
     void crearUsuario(UsuarioDTO usuarioNuevo);
+    void crearReserva();
+    void crearEmpleado();
+    
+
 }
