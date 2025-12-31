609694f09be9935835e56376406fc72f888d5acb
JosueOrd <josueg.ordonez@ucuenca.edu.ec>
2025-12-31 18:00:10 -0500

    Sin comentarios

--- src/main/java/com/reservaVuelos/modelo/vuelo/ModeloAvion.java
+++ src/main/java/com/reservaVuelos/modelo/vuelo/ModeloAvion.java
@@ -1,22 +1,19 @@
 package com.reservaVuelos.modelo.vuelo;
 
 public enum ModeloAvion {
-    // 1. Definimos las constantes con el número de asientos entre paréntesis
+
     AIRBUS_A320(150),
     BOEING_737(180),
     BOEING_707(140),
     BOEING_767(250),
     BOEING_787(300);
 
-    // 2. Variable para almacenar la cantidad de asientos
     private final int capacidadAsientos;
 
-    // 3. Constructor del Enum
     ModeloAvion(int capacidadAsientos) {
         this.capacidadAsientos = capacidadAsientos;
     }
 
-    // 4. Método Getter para obtener la cantidad de asientos
     public int getCapacidadAsientos() {
         return capacidadAsientos;
     }
