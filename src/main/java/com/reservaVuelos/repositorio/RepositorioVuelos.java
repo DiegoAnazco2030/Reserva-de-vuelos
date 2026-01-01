c67eb33453e1b136994d23fb7718491fa81d07e8
LeonardoByte <kirman.sanchez@ucuenca.edu.ec>
2025-12-31 19:55:42 -0500

    terminar de hacer el merge de mi compu

--- src/main/java/com/reservaVuelos/repositorio/RepositorioVuelos.java
+++ src/main/java/com/reservaVuelos/repositorio/RepositorioVuelos.java
@@ -4,12 +4,12 @@ public class RepositorioVuelos {
         // Tamaños fijos en bytes
     private static final int VUELO_ID_SIZE = 7;
     private static final int AEROLINEA_SIZE = 19;
-    private static final int ORIGEN_VUELO_SIZE = 9;
-    private static final int DESTINO_VUELO_SIZE = 9;
+    private static final int ORIGEN_VUELO_SIZE = 12;
+    private static final int DESTINO_VUELO_SIZE = 12;
     private static final int FECHA_HORA_SALIDA_SIZE = 29;
     private static final int FECHA_HORA_LLEGADA_SIZE = 29;
     private static final int ESTADO_VUELO_SIZE = 1;
     private static final int AVION_VUELO_SIZE = 7;
     
-    private static final int TOTAL_RECORD_SIZE = 111; // bytes por registro
+    private static final int TOTAL_RECORD_SIZE = 117; // bytes por registro
 }
