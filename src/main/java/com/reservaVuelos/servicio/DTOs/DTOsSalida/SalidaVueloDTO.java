e24374e958d8ce15b4cbccf2aaba2226602f6c9b
JosueOrd <josueg.ordonez@ucuenca.edu.ec>
2025-12-31 19:53:44 -0500

    Dtos completados

--- src/main/java/com/reservaVuelos/servicio/DTOs/DTOsSalida/SalidaVueloDTO.java
+++ src/main/java/com/reservaVuelos/servicio/DTOs/DTOsSalida/SalidaVueloDTO.java
@@ -4,9 +4,9 @@ import com.reservaVuelos.modelo.vuelo.Ciudad;
 import com.reservaVuelos.modelo.vuelo.ModeloAvion;
 
 public record SalidaVueloDTO(Ciudad origen,
-                             Ciudad destino,
-                             String salidaVuelo,
-                             String llegadaVuelo,
-                             String estadoVuelo,
-                             ModeloAvion modeloAvion) {
+     Ciudad destino,
+     String salidaVuelo,
+     String llegadaVuelo,
+     String estadoVuelo,
+     ModeloAvion modeloAvion) {
 }
