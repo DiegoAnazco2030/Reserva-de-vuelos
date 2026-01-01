1460774c88c1d21d62d5b7254e0efba3fa00d896
LeonardoByte <kirman.sanchez@ucuenca.edu.ec>
2026-01-01 17:39:49 -0500

    Calculo del tamaño de bytes de Usuario y Empleado, para repos diferentes.

--- src/main/java/com/reservaVuelos/modelo/vuelo/Vuelo.java
+++ src/main/java/com/reservaVuelos/modelo/vuelo/Vuelo.java
@@ -5,14 +5,14 @@ import com.reservaVuelos.modelo.Identificador;
 import java.time.LocalDateTime;
 
 public class Vuelo implements Identificador {
-    private final Long vueloID; // 7 caracteres (10^7 = 10,000,000 posibles IDs)
+    private final Long vueloID; // tamaño de Long
     private Aerolinea aerolinea; // 19 caracteres
     private Ciudad origenVuelo; // 12 caracteres
     private Ciudad destinoVuelo; // 12 caracteres
     private LocalDateTime fechaHoraSalida; // 29 caracteres
     private LocalDateTime fechaHoraLlegada; // 29 caracteres
-    private boolean estadoVuelo; //"0 = Programado", "1 = Aterrizado" / 1 caracter
-    private Avion avionVuelo; // 7 caracteres (ID del avión)
+    private boolean estadoVuelo; //"0 = Programado", "1 = Aterrizado" / tamaño de boolean
+    private Avion avionVuelo; // ID del avion / tamaño de Long
 
     public Vuelo(Long vueloID, Aerolinea aerolinea, Ciudad origenVuelo, Ciudad destinoVuelo, LocalDateTime fechaHoraSalida, LocalDateTime fechaHoraLlegada, boolean estadoVuelo, Avion avionVuelo) {
         this.vueloID = vueloID;
