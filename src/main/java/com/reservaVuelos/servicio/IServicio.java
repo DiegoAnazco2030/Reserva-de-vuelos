abe2455711328de23782a219b3e847cb4c30eb76
LeonardoByte <kirman.sanchez@ucuenca.edu.ec>
2026-01-02 16:21:49 -0500

    Documentacion para el respositorio del servicios

--- src/main/java/com/reservaVuelos/servicio/IServicio.java
+++ src/main/java/com/reservaVuelos/servicio/IServicio.java
@@ -1,16 +1,23 @@
 package com.reservaVuelos.servicio;
 
-import com.reservaVuelos.modelo.Identificador;
-import com.reservaVuelos.servicio.DTOs.DTOsCrear.CrearUsuarioDTO;
-
 import java.util.List;
 
 public interface IServicio<I, O> {
 
+    // ----------------------- CRUD ----------------------
+
+    // Crear
     void crear(I objeto);
+
+    // Leer todo los datos
     List<O> obtenerTodos();
-    void eliminar(Long id) throws Exception;
-    void modificar(Long id, I objeto) throws Exception;
+    // Leer por busqueda especifica
     List<O> obtenerListaReducida(String palabraBuscar);
+
+    // Eliminar por id
+    void eliminar(Long id) throws Exception;
+
+    // Actualizar por id
+    void modificar(Long id, I objeto) throws Exception;
 
 }
