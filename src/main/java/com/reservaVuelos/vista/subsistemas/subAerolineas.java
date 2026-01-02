087afa81dd087b8b7b6a3d9a3e9e7de3270422f0
Fractzone <diego937292@gmail.com>
2026-01-02 16:31:42 -0500

    Agrego mas swing

--- src/main/java/com/reservaVuelos/vista/subsistemas/subAerolineas.java
+++ src/main/java/com/reservaVuelos/vista/subsistemas/subAerolineas.java
@@ -1,13 +1,17 @@
 package com.reservaVuelos.vista.subsistemas;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class subAerolineas extends JFrame {
 
     private JPanel JPanelAero;
 
-    //Tabla que muestra las aerolienas registradas
+    //Tabla que muestra las aerolienas registradas y la configuracion
     private JTable tablaAerolinea;
+    private DefaultTableModel modeloTablaAerolinea;
 
     //Mis text fields de ingreso y busqueda
     private JTextField nombreAerolinea;
@@ -16,7 +20,7 @@ public class subAerolineas extends JFrame {
     private JTextField textFiBuscarAerolinea;
 
     //Bottoms de Crud y buscar
-    private JButton registrarModificarAerolinea;
+    private JButton registrarModificarAerolineas;
     private JButton buscarAerolinea;
     private JButton EliminarAerolinea;
 
@@ -31,6 +35,105 @@ public class subAerolineas extends JFrame {
         pack();
         setLocationRelativeTo(null);
 
+        /*Esta funcion es para que se pueda seleccionar y deseleccionar de la tabla y dependiendo
+        que si esta selecciona es modificar y si no lo esta es registrar*/
+        tablaAerolinea.getSelectionModel().addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                if (tablaAerolinea.getSelectedRow() == -1) {
+                    registrarModificarAerolineas.setText("Registrar");
+                    limpiarFormulario();
+                } else {
+                    registrarModificarAerolineas.setText("Modificar");
+                    cargarCamposDesdeTabla();
+                }
+            }
+        });
 
+
+        //Action listeners del buscar
+
+        textFiBuscarAerolinea.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        //Action listeners de los bottoms
+
+        buscarAerolinea.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        registrarModificarAerolineas.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String modo = registrarModificarAerolineas.getText();
+
+                if (modo.equals("Registrar")) {
+                    // Lógica para capturar campos y enviar al service.save()
+                    System.out.println("Registrando nueva aerolínea...");
+                } else {
+                    // Lógica para capturar el ID de la fila y enviar al service.update()
+                    int fila = tablaAerolinea.getSelectedRow();
+                    Object id = modeloTablaAerolinea.getValueAt(fila, 0);
+                    System.out.println("Modificando aerolínea con ID: " + id);
+                }
+            }
+        });
+
+        EliminarAerolinea.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+    }
+
+    //Carga los datos seleccionados en la tabla en el formulario de forma automatica
+    private void cargarCamposDesdeTabla() {
+        int fila = tablaAerolinea.getSelectedRow();
+        if(fila != -1) {
+            nombreAerolinea.setText(modeloTablaAerolinea.getValueAt(fila, 1).toString());
+            telefonoAerolinea.setText(modeloTablaAerolinea.getValueAt(fila, 2).toString());
+            emailAerolinea.setText(modeloTablaAerolinea.getValueAt(fila, 3).toString());
+        }
+    }
+
+    //Cuando se deseleccionar la tabla se limpia el fomrulario
+    private void limpiarFormulario() {
+        nombreAerolinea.setText("");
+        telefonoAerolinea.setText("");
+        emailAerolinea.setText("");
+        mensajeService.setText(""); // Limpiar avisos
+    }
+
+    private void createUIComponents() {
+        tablaAerolinea = new JTable() {
+            @Override
+            public void changeSelection(int rowIndex, int columnIndex, boolean toggle, boolean extend) {
+                if (isRowSelected(rowIndex) && isColumnSelected(columnIndex)) {
+                    clearSelection();
+                } else {
+                    super.changeSelection(rowIndex, columnIndex, toggle, extend);
+                }
+            }
+        };
+
+        modeloTablaAerolinea = new DefaultTableModel(){
+            @Override
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return false;
+            }
+        };
+        modeloTablaAerolinea.addColumn("ID");
+        modeloTablaAerolinea.addColumn("Nombre");
+        modeloTablaAerolinea.addColumn("Telefono");
+        modeloTablaAerolinea.addColumn("Email");
+        tablaAerolinea.setModel(modeloTablaAerolinea);
     }
 }
--- src/main/java/com/reservaVuelos/vista/subsistemas/subAviones.java
+++ src/main/java/com/reservaVuelos/vista/subsistemas/subAviones.java
@@ -1,4 +1,119 @@
 package com.reservaVuelos.vista.subsistemas;
 
-public class subAviones {
+import com.reservaVuelos.modelo.vuelo.ModeloAvion;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class subAviones extends JFrame {
+
+    private JPanel JPanelAvion;
+
+    //Los text field para la busqueda
+    private JTextField textFiBusquedaAvion;
+
+    //Los bottom para el crud
+    private JButton buscarAvion;
+    private JButton registrarModificarAvion;
+    private JButton eliminarAvion;
+
+    //Tabla de aviones
+    private JTable tablaAviones;
+    private DefaultTableModel modeloTablaAviones;
+
+    //Combo Box del modelo de avion
+    private JComboBox modeloAvion;
+
+    public subAviones() {
+
+        setContentPane(JPanelAvion);
+        setTitle("Registro de avion");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+
+        DefaultComboBoxModel<ModeloAvion> modelModeloAvion = new DefaultComboBoxModel<>(ModeloAvion.values());
+        modeloAvion.setModel(modelModeloAvion);
+
+        /*Esta funcion es para que se pueda seleccionar y deseleccionar de la tabla y dependiendo
+        que si esta selecciona es modificar y si no lo esta es registrar*/
+        tablaAviones.getSelectionModel().addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                if (tablaAviones.getSelectedRow() == -1) {
+                    registrarModificarAvion.setText("Registrar");
+                } else {
+                    registrarModificarAvion.setText("Modificar");
+                    cargarCamposDesdeTabla();
+                }
+            }
+        });
+
+        //Listener del text field de buscar
+
+        textFiBusquedaAvion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        //Listener de los bottoms
+
+        buscarAvion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        registrarModificarAvion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        eliminarAvion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    //Carga los datos seleccionados en la tabla en el formulario de forma automatica
+    private void cargarCamposDesdeTabla() {
+        int fila = tablaAviones.getSelectedRow();
+        if(fila != -1) {
+            String modeloSeleccionado = modeloTablaAviones.getValueAt(fila, 1).toString();
+            modeloAvion.setSelectedItem(ModeloAvion.valueOf(modeloSeleccionado));
+        }
+    }
+
+    private void createUIComponents() {
+        tablaAviones = new  JTable(){
+
+            @Override
+            public void changeSelection(int rowIndex, int columnIndex, boolean toggle, boolean extend) {
+                if (isRowSelected(rowIndex) && isColumnSelected(columnIndex)) {
+                    clearSelection();
+                } else {
+                    super.changeSelection(rowIndex, columnIndex, toggle, extend);
+                }
+            }
+        };
+
+        modeloTablaAviones = new DefaultTableModel(){
+            @Override
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return false;
+            }
+        };
+        modeloTablaAviones.addColumn("ID");
+        modeloTablaAviones.addColumn("Modelo");
+        modeloTablaAviones.addColumn("Capacidad");
+        tablaAviones.setModel(modeloTablaAviones);
+    }
 }
